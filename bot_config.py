bebebecb27ea46b7fc484feadc3ec10a2a94d3e5
JoanZatafa <joan.aleonard@gmail.com>
2021-11-07 20:31:41 +0100

    Update bot_config.py

--- bot_config.py
+++ bot_config.py
@@ -11,6 +11,8 @@ class DefaultConfig:
 
     ############## Azure Bot Service ###############
     PORT = 3978
+    # APP_ID = os.environ.get("MicrosoftAppId", "") 
+    # APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "")
     APP_ID = os.environ.get("MicrosoftAppId", "f5f366ba-253d-4e54-b69d-a476d2ba57cf") 
     APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "QZIj3-W6IKOI>nG_1KxmG_MvjN12$.kg")
 
